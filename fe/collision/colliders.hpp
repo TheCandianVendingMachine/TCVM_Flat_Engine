1afb5397730a6cb45758519facb74be55fb35126
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-03-01 10:59:19 -0700

    Removed logic from collider code. Colliders are now generic

---
+++ fe/collision/colliders.hpp
@@ -0,0 +1,22 @@
+// colliders.hpp
+// all colliders in the game
+#pragma once
+#include "../math/Vector2.hpp"
+
+namespace fe
+    {
+        struct collider
+            {
+                Vector2d &m_position;
+
+                collider(Vector2d &position) : m_position(position) {}
+            };
+
+        struct AABB : public collider
+            {
+                AABB(Vector2d &position) : collider(position) {}
+
+                Vector2d m_min;
+                Vector2d m_max;
+            };
+    }
