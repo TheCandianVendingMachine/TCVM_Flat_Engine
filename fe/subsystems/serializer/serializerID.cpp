2fb7f595c92d74bdde8798c7d961e66b67bf4385
Brandon Danyluk <brandondany@hotmail.co.uk>
2017-04-12 11:36:16 -0600

    Added different serialization classes. All specialize in their own thing.

---
+++ fe/subsystems/serializer/serializerID.cpp
@@ -0,0 +1,109 @@
+#include "serializerID.hpp"
+
+void serializerID::dataBlock::outData(std::ostream &out)
+    {
+        out << m_id << "{\n";
+        for (auto &dat : m_mappedData)
+            {
+            out << dat.first << ":" << dat.second << ";\n";
+            }
+        out << "}\n";
+    }
+
+void serializerID::dataBlock::readData(const std::string &block)
+    {
+        enum readState
+            {
+                ID,
+                DATA,
+                END
+            };
+        readState curState = readState::ID;
+
+        std::string line;
+        unsigned int index = 0;
+        bool readBlock = true;
+
+        while (readBlock)
+            {
+                bool foundEOL = false;
+                while (!foundEOL)
+                    {
+                        if (block[index] != ';' && block[index] != '{' && block[index] != '}')
+                            {
+                                line += block[index];
+                            }
+                        else
+                            {
+                                foundEOL = true;
+                            }
+
+                        if (index++ >= block.size())
+                            {
+                                return;
+                            }
+                    }
+
+                switch (curState)
+                    {
+                        case ID:
+                            {                
+                                m_id = line;
+                                curState = readState::DATA;
+                                line.clear();
+                            }
+                            break;
+                        case DATA:
+                            {
+                                std::string datID;
+                                std::string datStr;
+                                bool findingID = true;
+                                for (int i = 0; i < line.size(); i++)
+                                    {
+                                        if (line[i] == '}')
+                                            {
+                                                curState = readState::END;
+                                                break;
+                                            }
+                                        else if (line[i] == ':')
+                                            {
+                                                findingID = false;
+                                            }
+                                        else if (findingID)
+                                            {
+                                                datID += line[i];
+                                            }
+                                        else
+                                            {
+                                                datStr += line[i];
+                                            }
+                                    }
+                                line.clear();
+                                m_mappedData[datID] = datStr;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+            }
+    }
+
+void serializerID::readData(std::istream &in)
+    {
+        m_data.clear();
+
+        std::string datBlock;
+        std::string line;
+        while (std::getline(in, line))
+            {
+                datBlock += line;
+                if (line == "}")
+                    {
+                        dataBlock newData;
+                        newData.readData(datBlock);
+                        datBlock.clear();
+
+                        m_data.push_back(newData);
+                    }
+            }
+    }
---
+++ fe/subsystems/serializer/serializerID.hpp
@@ -0,0 +1,143 @@
+// serializerID
+// Allows the user to serialize data to a stream with IDs to
+// get and set data easily
+#pragma once
+#include <ostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class serializerID
+    {
+        protected:
+            struct dataBlock 
+                {
+                    std::string m_id;
+                    std::unordered_map<std::string, std::string> m_mappedData;
+                    bool m_read; // if this block has already been read, we dont want
+                                 // to get data from it again
+
+                    dataBlock() : m_id(""), m_read(false) {}
+
+                    void outData(std::ostream &out);
+                    void readData(const std::string &block);
+                };
+
+            std::vector<dataBlock> m_data;
+
+            template<typename T>
+            T convertValue(const std::string &in, T val) { return T(); }
+            template<>
+            int convertValue(const std::string &in, int val);
+
+        public:
+            void serialize() 
+                {}
+
+            template<typename T>
+            void serialize(dataBlock &block, const std::string &id, T &&data);
+
+            template<typename T, typename ...Args>
+            void serialize(dataBlock &block, const std::string &id, T &&data, Args &&...args);
+
+            template<typename ...Args>
+            void serializeBlock(const std::string &blockID, Args &&...args);
+
+            void deserialize() {}
+            template<typename T>
+            void deserialize(dataBlock &dataBlock, const std::string &id, T &newValue);
+
+            template<typename T, typename ...Args>
+            void deserialize(dataBlock &dataBlock, const std::string &id, T &newValue, Args &&...args);
+
+            template<typename T, typename ...Args>
+            void deserializeBlock(const std::string &blockID, const std::string &id, T &newValue, Args &&...args);
+
+            void outData(std::ostream &out)
+                {
+                    for (auto &data : m_data)
+                        {
+                            data.outData(out);
+                        }
+                }
+
+            void readData(std::istream &in);
+    };
+
+#define SERIALIZE_ID(...) \
+void serialize(serializerID &serial) { serial.serializeBlock(__VA_ARGS__); }\
+void deserialize(serializerID &serial) { serial.deserializeBlock(__VA_ARGS__); }
+
+template<>
+inline int serializerID::convertValue(const std::string &in, int val)
+    {
+        return std::stoi(in);
+    }
+
+template<>
+inline float serializerID::convertValue(const std::string &in, float val)
+    {
+        return std::stof(in);
+    }
+
+template<>
+inline double serializerID::convertValue(const std::string &in, double val)
+    {
+        return std::stod(in);
+    }
+
+template<typename T>
+void serializerID::serialize(dataBlock &block, const std::string &id, T &&data)
+    {
+        block.m_mappedData[id] = std::to_string(data);
+        int i = 0;
+    }
+
+template<typename T, typename ...Args>
+void serializerID::serialize(dataBlock &block, const std::string &id, T &&data, Args &&...args)
+    {
+        serialize(block, id, data);
+        serialize(block, std::forward<Args>(args)...);
+    }
+
+template<typename ...Args>
+void serializerID::serializeBlock(const std::string &blockID, Args &&...args)
+    {
+        dataBlock block;
+        block.m_id = blockID;
+        serialize(block, args...);
+        m_data.push_back(block);
+    }
+
+template<typename T>
+void serializerID::deserialize(dataBlock &dataBlock, const std::string &id, T &newValue)
+    {
+        newValue = convertValue(dataBlock.m_mappedData[id], newValue);
+        int i = 0;
+    }
+
+template<typename T, typename ...Args>
+void serializerID::deserialize(dataBlock &dataBlock, const std::string &id, T &newValue, Args &&...args)
+    {
+        deserialize(dataBlock, id, newValue);
+        deserialize(dataBlock, std::forward<Args>(args)...);
+    }
+
+template<typename T, typename ...Args>
+void serializerID::deserializeBlock(const std::string &blockID, const std::string &id, T &newValue, Args &&...args)
+    {
+        dataBlock *selectedBlock = nullptr;
+        for (auto &datBlock : m_data)
+            {
+                if (datBlock.m_id == blockID && !datBlock.m_read)
+                    {
+                        selectedBlock = &datBlock;
+                    }
+            }
+
+        if (selectedBlock) 
+            {
+                selectedBlock->m_read = true;
+                deserialize(*selectedBlock, id, newValue, args...);
+            }
+    }
