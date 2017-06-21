c62f2f338f51f31243b8fb99c9af22c8e1211856
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-06-20 18:22:30 -0600

    Moved fe::Transformable loction. Added rigid body definition

---
+++ fe/subsystems/physics/rigidBody.hpp
@@ -0,0 +1,53 @@
+// rigidBody.hpp
+// Simple rigid body. Can move
+#pragma once
+#include "transformable.hpp"
+#include "../../math/Vector2.hpp"
+
+namespace fe
+    {
+        class rigidBody : public fe::transformable
+            {
+                private:
+                    // Current velocity of the object during this frame
+                    fe::Vector2d m_velocity;
+                    // Current force being applied next frame
+                    fe::Vector2d m_force;
+
+                    // Max speed object can go
+                    float m_maxSpeed;
+                    // How heavy the object is
+                    float m_mass;
+                    // How much the object will be affected by friction
+                    float m_frictionCoeff; // unit float for friction. 1 = Stops immediately -- 0 = Never Slows Down
+
+                public:
+                    rigidBody(float mass);
+                    rigidBody(float mass, float maxSpeed);
+                    rigidBody(float mass, float maxSpeed, float frictionCoeff);
+
+                    void setMass(float newMass);
+                    void setMaxSpeed(float maxSpeed);
+                    void setFrictionCoefficient(float fricCoeff);
+
+                    // applies a force upon the rigid body. 
+                    void applyForce(fe::Vector2d force);
+
+                    void setVelocity(fe::Vector2d newVel);
+                    void setForce(fe::Vector2d newForce);
+                    void setDirection(fe::Vector2d newDirection);
+
+                    fe::Vector2d getVelocity() const;
+                    fe::Vector2d getForce() const;
+                    fe::Vector2d getDirection() const;
+
+                    float getSpeed() const;
+                    float getTotalForce() const;
+                    float getHeading() const;
+
+                    float getMass() const;
+                    float getMaxVelocity() const;
+                    float getFrictionCoefficient() const;
+
+            };
+    }
