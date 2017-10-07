6cf340709d4e7b235c4e2671a80cff45c7fd89a3
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-10-06 20:23:16 -0600

    Added ability to load a XML document as a translation table. Translation table has very specific needs

--- fe/localization/localizationStorage.cpp
+++ fe/localization/localizationStorage.cpp
@@ -4,32 +4,85 @@
 #include <rapidxml_print.hpp>
 #include <iostream>
 
+void fe::localizationStorage::load(rapidxml::xml_node<> *currentNode)
+    {
+        if (!currentNode) return;
+
+        for (rapidxml::xml_node<> *nextNode = currentNode; nextNode; nextNode = nextNode->first_node())
+            {
+                std::string name = nextNode->name();
+                if (name == "Package")
+                    {
+                        #if !COMPILE_FULL_STACK
+                            for (rapidxml::xml_node<> *idNodes = nextNode->first_node(); idNodes; idNodes = idNodes->next_sibling())
+                                {
+                                    std::string name = idNodes->name();
+                                    if (name == "strId")
+                                        {
+                                            std::string id = idNodes->first_attribute()->value();
+                                            for (rapidxml::xml_node<> *sibling = idNodes->first_node(); sibling; sibling = sibling->next_sibling())
+                                                {
+                                                    std::string language = sibling->first_attribute()->value();
+                                                    std::string content = sibling->value();
+                                                    m_text[FE_STR(nextNode->first_attribute()->value())][FE_STR(id.c_str())][FE_STR(language.c_str())] = content;
+                                                }
+                                        }
+                                }
+                        #else
+                            loadPackage(nextNode, FE_STR(nextNode->first_attribute()->value()));
+                        #endif
+                    }
+            }
+    }
+
+void fe::localizationStorage::loadPackage(rapidxml::xml_node<> *packageBaseNode, fe::guid package)
+    {
+        for (rapidxml::xml_node<> *idNodes = packageBaseNode->first_node(); idNodes; idNodes = idNodes->next_sibling())
+            {
+                std::string name = idNodes->name();
+                if (name == "strId")
+                    {
+                        #if !COMPILE_FULL_STACK
+                            std::string id = idNodes->first_attribute()->value();
+                            for (rapidxml::xml_node<> *sibling = idNodes->first_node(); sibling; sibling = sibling->next_sibling())
+                                {
+                                    std::string language = sibling->first_attribute()->value();
+                                    std::string content = sibling->value();
+                                    m_text[package][FE_STR(id.c_str())][FE_STR(language.c_str())] = content;
+                                }
+                        #else
+                            loadID(idNodes, package);
+                        #endif
+                    }
+            }
+    }
+
+void fe::localizationStorage::loadID(rapidxml::xml_node<> *idNode, fe::guid package)
+    {
+        std::string id = idNode->first_attribute()->value();
+        for (rapidxml::xml_node<> *sibling = idNode->first_node(); sibling; sibling = sibling->next_sibling())
+            {
+                std::string language = sibling->first_attribute()->value();
+                std::string content = sibling->value();
+                m_text[package][FE_STR(id.c_str())][FE_STR(language.c_str())] = content;
+            }
+    }
+
 void fe::localizationStorage::load(const char *textFilepath)
     {
         std::string content;
         auto xmlDoc = fe::loadFile(textFilepath, content);
 
-        std::string id = "";
-        for (rapidxml::xml_node<> *node = xmlDoc->first_node(); node; node = node->first_node())
+        // Loop through all packages
+        for (rapidxml::xml_node<> *node = xmlDoc->first_node()->first_node(); node; node = node->next_sibling())
             {
-                for (rapidxml::xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute())
-                    {
-                        std::string name = attr->name();
-                        if (name == "ID")
-                            {
-                                id = attr->value();
-                            }
-                        else if (name == "language")
-                            {
-                                m_text[FE_STR(id.c_str())][FE_STR(attr->value())] = node->value();
-                            }
-                    }
+                load(node);
             }
 
         fe::freeDocument(xmlDoc);
     }
 
-std::string fe::localizationStorage::get(fe::guid textID, fe::guid language)
+std::string fe::localizationStorage::get(fe::guid packageID, fe::guid textID, fe::guid language)
     {
-        return m_text[textID][language];
+        return m_text[packageID][textID][language];
     }
--- fe/localization/localizationStorage.hpp
+++ fe/localization/localizationStorage.hpp
@@ -2,22 +2,34 @@
 // All text that is localized is stored here. Contains std::unordered_map with a string and a map containing languages/text
 #pragma once
 #define FLAT_ENGINE_EXPORT
-#include "../flatEngineExport.hpp"
+#define COMPILE_FULL_STACK false
 
+#include "../flatEngineExport.hpp"
 #include "../typeDefines.hpp"
 #include <string>
 #include <unordered_map>
+
+namespace rapidxml
+    {
+        template<class Ch = char>
+        class xml_node;
+    }
 
 namespace fe
     {
         class localizationStorage
             {
                 private:
-                    std::unordered_map<fe::guid, std::unordered_map<fe::guid, std::string>> m_text;
+                    // m_text[package][id][language] = text
+                    std::unordered_map<fe::guid, std::unordered_map<fe::guid, std::unordered_map<fe::guid, std::string>>> m_text;
+
+                    FLAT_ENGINE_API void load(rapidxml::xml_node<> *currentNode);
+                    FLAT_ENGINE_API void loadPackage(rapidxml::xml_node<> *packageBaseNode, fe::guid package); // Not used if COMPILE_FULL_STACK is false
+                    FLAT_ENGINE_API void loadID(rapidxml::xml_node<> *idNode, fe::guid package); // Not used if COMPILE_FULL_STACK is false
 
                 public:
                     FLAT_ENGINE_API void load(const char *textFilepath);
-                    FLAT_ENGINE_API std::string get(fe::guid textID, fe::guid language);
+                    FLAT_ENGINE_API std::string get(fe::guid packageID, fe::guid textID, fe::guid language);
 
             };
     }
