a9e0504a7d04d5807550ca82fc91b627f11de4f9
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2018-03-30 02:09:31 -0600

    Friction is difficult. Should be fixed for now

--- fe/subsystems/physics/physicsEngine.cpp
+++ fe/subsystems/physics/physicsEngine.cpp
@@ -8,7 +8,7 @@
 
 fe::physicsEngine::physicsEngine() :
     m_gravityForceX(0.f),
-    m_gravityForceY(0.f),
+    m_gravityForceY(10.f),
     m_gravityForceZ(0.f),
     m_maxObjectsUntilThread(5000) // 5000 = ~when intersection of FPS occurs between threaded physics and not
     {
@@ -62,22 +62,24 @@ void fe::physicsEngine::simulateForces(float deltaTime, unsigned int iterations)
                         float normalForceY = abs(body->getMass() * m_gravityForceY);
                         float normalForceZ = abs(body->getMass() * m_gravityForceZ);
 
-                        float friction = body->getFrictionCoefficient() * normalForceZ;
+                        float frictionY = body->getFrictionCoefficient() * normalForceY;
+                        float frictionX = body->getFrictionCoefficient() * normalForceX;
+                        float frictionZ = body->getFrictionCoefficient() * normalForceZ;
 
                         float forceX = normalForceX;
                         float forceY = normalForceY;
 
-                        float frictionForceX = (-friction * (body->getForceX() / bodyTotalForce));
-                        float frictionForceY = (-friction * (body->getForceY() / bodyTotalForce));
+                        float frictionForceX = (-frictionZ * (body->getForceX() / bodyTotalForce));
+                        float frictionForceY = (-frictionZ * (body->getForceY() / bodyTotalForce));
 
-                        if (abs(frictionForceX + forceX) > abs(body->getForceX() + body->getImpulseX()))
+                        if (frictionForceX > 0.f && abs(frictionForceX + forceX) > abs(body->getForceX()))
                             {
-                                frictionForceX = forceX - body->getForceX();
+                                frictionForceX = body->getForceX() - forceX;
                             }
 
-                        if (abs(frictionForceY + forceY) > abs(body->getForceY() + body->getImpulseY()))
+                        if (frictionForceY > 0.f && abs(frictionForceY + forceY) > abs(body->getForceY()))
                             {
-                                frictionForceY = forceY - body->getForceY();
+                                frictionForceY = body->getForceY() - forceY;
                             }
 
                         forceX += frictionForceX;
