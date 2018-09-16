3ff6fafba87666580cc52359fe3fc5e1266392ca
Brandon-PC\Brandon <brandondanyluk366@gmail.com>
2018-09-15 22:19:30 -0600

    fix typo

--- src/fe/subsystems/collision/bounds/mixedTests.cpp
+++ src/fe/subsystems/collision/bounds/mixedTests.cpp
@@ -9,7 +9,7 @@ bool fe::intersects(const fe::AABB &a, const fe::circle &b)
     {
         fe::Vector2d point = fe::closestPoint(a, b.m_globalPositionX, b.m_globalPositionY);
 
-        fe::Vector2d v = point - fe::Vector2d(a.m_globalPositionX, a.m_globalPositionY);
+        fe::Vector2d v = point - fe::Vector2d(b.m_globalPositionX, b.m_globalPositionY);
         return v.dot(v) <= (b.m_radius * b.m_radius);
     }
 
